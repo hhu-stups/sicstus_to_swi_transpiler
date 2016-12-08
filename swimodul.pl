68ad38e15938c896ae0ae3d97c7a7ca83e1f04bb
Daniel Laps <daniel.laps@hhu.de>
2016-12-08 11:57:04 +0100

    DCG try and start string translation

--- swimodul.pl
+++ swimodul.pl
@@ -3,4 +3,12 @@
 :- use_module(sicstuscode).
 
 test(X) :-
-    get_empty(X).
+  get_empty(X),
+  test_strings.
+
+test_strings :-
+  A = "test double quotes",
+  write(A).
+
+b --> "test", {X="test", write(X)}.
+b --> "test", b.
--- transpiler.pl
+++ transpiler.pl
@@ -1,14 +1,21 @@
 do_expand((:-use_module(library(avl))), (:-use_module(library(assoc)))).
 do_expand((A:-B), (A:-NB)) :-
-    expand_body(B,NB).
+  expand_body(B,NB).
 
 expand_body((A,B),(NA,NB)) :- !,
-    expand_body(A,NA), expand_body(B,NB).
-
-expand_body(empty_avl(X),empty_assoc(X)) :- !.
-
+  expand_body(A,NA), expand_body(B,NB).
+expand_body((A,B),(NA,NB)) :- !,
+  expand_body(A,NA), expand_body(B,NB).
+expand_body(A=B,A=NB) :- !,
+  expand_expression(B, NB).
+expand_body(empty_avl(X), empty_assoc(X)) :- !.
 expand_body(A,A).
 
+expand_expression(String, List) :-
+  string(String),
+  string_codes(String, List).
+expand_expression(A, A).
+
 term_expansion(A, B) :-
-    do_expand(A,B),
-    format('from ~w to ~w\n',[A,B]).
+  do_expand(A,B),
+  format('from ~w to ~w\n',[A,B]).
