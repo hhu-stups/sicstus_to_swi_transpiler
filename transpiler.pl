5e27b81df98d603a62b747ed04648ab3d406673a
Sebastian Krings <sebastian@krin.gs>
2016-12-16 14:18:39 +0100

    add missing expansion rules

--- transpiler.pl
+++ transpiler.pl
@@ -1,11 +1,14 @@
 do_expand((:-use_module(library(avl))), (:-use_module(library(assoc)))).
 do_expand((A:-B), (A:-NB)) :-
   expand_body(B,NB).
+do_expand((A-->B), (A-->NB)) :-
+  expand_body(B,NB).
+do_expand(A,A).
 
 expand_body((A,B),(NA,NB)) :- !,
   expand_body(A,NA), expand_body(B,NB).
-expand_body((A,B),(NA,NB)) :- !,
-  expand_body(A,NA), expand_body(B,NB).
+expand_body({A},{NA}) :- !,
+  expand_body(A,NA).
 expand_body(A=B,A=NB) :- !,
   expand_expression(B, NB).
 expand_body(empty_avl(X), empty_assoc(X)) :- !.
